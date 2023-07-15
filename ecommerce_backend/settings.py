71dc9c3df3f38f5201c657016fcd173caa8cbfd7
Prajyot Khadse <yopro1999@gmail.com>
2023-07-15 19:10:54 +0530

    Update settings.py

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['deployecommbackend-production.up.railway.app', 'localhost', '127.0.0.1']
 # ALLOWED_HOSTS = ['*']
