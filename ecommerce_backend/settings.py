f1cec81a32bccf2258fe55d986ab211499f43a90
Prajyot Khadse <yopro1999@gmail.com>
2023-07-15 18:25:29 +0530

    Update settings.py

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -195,7 +195,7 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "https://rajyo.github.io/",
+    "https://deploy-ecomm-frontend.vercel.app/",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
